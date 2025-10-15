eafd679665c303ec401beab171fea1188b3b31ca
Santiago Talavera <santiago.a.talavera@gmail.com>
2025-10-15 16:26:39 -0300

    Uso de variables de entorno en authContext.jsx

--- React-ui-lib/libreria-bootstrap/frontend/src/context/authContext.jsx
+++ React-ui-lib/libreria-bootstrap/frontend/src/context/authContext.jsx
@@ -32,7 +32,7 @@ export function AuthProvider({ children }) {
   const login = async (email, password) => {
     try {
       // Mismo endpoint que ya usábamos
-      const res = await fetch("http://localhost:3000/api/auth/login", {
+      const res = await fetch(`${import.meta.env.VITE_API_URL || "http://localhost:3000"}/api/auth/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password })
